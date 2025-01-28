95133675fbf2811dc762df91b32ed02d3464c439
Daniel <Daniel.halaas@gmail.com>
2025-01-28 12:45:40 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,6 +10,11 @@ mail = Mail(app)
 def index():
     return render_template('index.html')
 
+@app.post('/')
+def speed_test():
+    results = test_speed()
+    return results
+
 
 if __name__ == "__main__":
     app.run(debug=True)
